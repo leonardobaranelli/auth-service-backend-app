a168e927fe3f733c427859a315e3219aecd3bc9a
leonardobaranelli <leobaranelli@gmail.com>
2024-01-30 14:35:58 -0300

    feat: User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Table, Model, DataType, Default, PrimaryKey } from "sequelize-typescript";
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+@Table({ tableName: 'User' })
+export class User extends Model<User> {
+    @Default(DataType.UUIDV4)
+    @PrimaryKey
+    @Column({ type: DataType.UUID, allowNull: false })
+    @ApiProperty()
+    id: string;
+
+    @Column({ allowNull: false })
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @Column({ allowNull: false, unique: true })
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @Column({ allowNull: false })
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+}
